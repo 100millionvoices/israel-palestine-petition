00aeb9c3771906de2e40aaa1cb16cf187ceff722
Jolyon Pawlyn <jpawlyn@gmail.com>
2017-03-06 20:27:24 +0100

    Only do ip lookup if constant is defined

--- app/models/geo_ip_lookup.rb
+++ app/models/geo_ip_lookup.rb
@@ -2,7 +2,7 @@ class GeoIpLookup
   GEOIP_DB = MaxMindDB.new(ENV.fetch('GEOIP_DB_PATH')) if File.exist?(ENV.fetch('GEOIP_DB_PATH'))
 
   def self.fetch_location_from_ip(ip)
-    return unless File.exist?(ENV.fetch('GEOIP_DB_PATH'))
+    return unless defined?(GEOIP_DB)
     lookup = GEOIP_DB.lookup(ip)
     lookup.found? ? lookup : nil
   end
