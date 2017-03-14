6b7b64e3dbecdd15c2206efce80fa1c1f76b85b0
Jolyon Pawlyn <jpawlyn@gmail.com>
2017-03-14 21:19:28 +0100

    Add language switcher

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  LOCALES = { 'de' => 'Deutsch', 'en' => 'English' }
+
+  def locales
+    LOCALES
+  end
+
+  def current_language
+    LOCALES[params[:locale]]
+  end
 end
