f4ac01b39b7ad85ee3998a95a995f66a7c7e120a
Jolyon Pawlyn <jpawlyn@gmail.com>
2017-04-10 23:20:54 +0200

    Add and amend translations

--- spec/features/confirm_signature_spec.rb
+++ spec/features/confirm_signature_spec.rb
@@ -13,6 +13,6 @@ feature 'Confirm signature' do
   scenario 'User fails to confirm a non-existent signature' do
     visit confirm_signatures_path(token: 'no-token', locale: :en)
 
-    expect(page).to have_text 'Sorry but we could not find your signature to confirm it'
+    expect(page).to have_text 'Your signture has already been confirmed'
   end
 end
